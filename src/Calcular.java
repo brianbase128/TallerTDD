da54d03f22f421f6448e2c4f48a04cc8f6f00ba1
Brian Andres Silva Espinosa <brianbase182@gmail.com>
2022-05-05 23:59:51 -0500

    GREEN-SUM- prueba exitosa el metodo sumar ya existe en la clase Calcular y compila

--- src/Calcular.java
+++ src/Calcular.java
@@ -1,4 +1,16 @@
 
 public class Calcular {
 
+	public Object valor() {
+		return 0;
+	}
+	
+	public void sumar(int i, int j) {
+		
+	}
+
+	
+
+
+
 }
--- src/CalcularTest.java
+++ src/CalcularTest.java
@@ -1,4 +1,6 @@
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,11 +9,13 @@ public class CalcularTest {
 	@Test
 	public void test() {
 		Calcular calc = new Calcular();
+		assertEquals(0, calc.valor());
 	}
 	
+	@Test
 	public void sumar() {
 		Calcular calc = new Calcular();
-		calc.sumar();
+		calc.sumar(1,1);
 	}
 	
 
