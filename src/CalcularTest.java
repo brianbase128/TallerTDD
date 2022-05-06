266da1799ed799f20d0c3222468e424905cf6fd9
Brian Andres Silva Espinosa <brianbase182@gmail.com>
2022-05-05 23:51:46 -0500

    RED-SUM- prueba fallida el metodo sumar no existe en la clase Calcular

--- src/CalcularTest.java
+++ src/CalcularTest.java
@@ -4,7 +4,15 @@ import org.junit.Test;
 
 public class CalcularTest {
 
-
+	@Test
+	public void test() {
+		Calcular calc = new Calcular();
+	}
+	
+	public void sumar() {
+		Calcular calc = new Calcular();
+		calc.sumar();
+	}
 	
 
 }
