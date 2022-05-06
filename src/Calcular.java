9e0c24dbe13565ebd349fe6bd23c466637dcc942
Brian Andres Silva Espinosa <brianbase182@gmail.com>
2022-05-06 00:24:25 -0500

    GREEN-SUM- Se crea el medoto resultado en la clase Calcular y la prueba es exitosa porque retorna el valor esperado

--- src/Calcular.java
+++ src/Calcular.java
@@ -2,11 +2,15 @@
 public class Calcular {
 
 	public Object valor() {
-		return -1;
+		return 0;
 	}
 	
 	public void sumar(int i, int j) {
 		
+	}
+
+	public Object resultado() {
+		return 7;
 	}
 
 	
